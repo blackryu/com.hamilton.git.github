1076cdd5252a3894d053def919a5ecefe36fe3d2
Cody Hamilton <cody.hamilton@gmail.com>
2014-10-29 14:05:17 +0100

    quickfix
    Signed-off-by: Cody Hamilton <cody.hamilton@gmail.com>

--- src/helloWorld/Hello.java
+++ src/helloWorld/Hello.java
@@ -2,9 +2,12 @@ package helloWorld;
 
 public class Hello {
 
+	public static void helloWorld(){
+		System.out.println("I now print outside of main, Still learning Egit");
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Learning EGit");
-
+		helloWorld();
 	}
 
 }
